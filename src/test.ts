1744c0b07e722ec1c69a47a8dab1796565cfd7fd
ICEHUB-ME <iceling.me@gmail.com>
2025-12-26 12:10:08 -0800

    New ts file, test.ts

---
+++ src/test.ts
@@ -0,0 +1,13 @@
+export const test = () => {
+    console.log("Test function executed.");
+
+    // const foo = (arg: string):string => {
+    //     return arg;
+    // }
+
+    const foo = <T>(arg: T): T => {
+        return arg;
+    }
+
+    foo<string>("Hello, World!");
+}
